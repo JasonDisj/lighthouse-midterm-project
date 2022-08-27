37ef8bd5f78b71d803c551a6afe129904101c08f
JasonDisj <jasondisj@gmail.com>
2022-08-27 19:06:51 +0000

    Listings by Jason

--- db/seeds/02_video_game_listings.sql
+++ db/seeds/02_video_game_listings.sql
@@ -1,4 +1,4 @@
-INSERT INTO video_game_listings (name, company, platform, description, price, cover_photo_rul, is_stocked, adminid) VALUES
+INSERT INTO video_game_listings (name, company, platform, description, price, cover_photo_url, is_stocked, admin_id) VALUES
 
 ('sengoku', 'capcom', 'wii', 'life is journey', 5000, 'https://en.wikipedia.org/wiki/File:Sengoku.jpg', 'photourl01', ture, 7)
 ('zelda', 'nintendo', 'nintenco', 'the die is cast', 6700, 'https://store.nintendo.co.kr/70010000009367', 'photourl02', true, 2)
@@ -10,3 +10,11 @@ INSERT INTO video_game_listings (name, company, platform, description, price, co
 ('resident evil', 'capcom', 'gamecube', 'habit is a second nature', 12000, 'http://buywhy.com/', 'photourl08', true, 15)
 ('super mario world', 'nintendo', 'super nintendo', 'time is gold', 1000, 'http://bookroad.com/', 'photourl09', true, 45)
 ('mario bros.3', 'nintendo', 'nes', 'time waits for no one', 3400, 'http://raincouver.com/', 'photourl10', false, 34);
+
+('Pokémon Legends: Arceus', 'Nintendo', 'Switch', 'Pokémon did not necessarily need to change. This game is sold extraordinarily well.', 5100, 'https://www.videogameschronicle.com/files/2021/08/pokemon-legends-arceus-1024x578.jpg', TRUE, 3);
+('Rainbow 6 Extraction', 'Ubisoft', 'PlayStation 5', 'You remember what it is like to truly fear death in Extraction.', 1200, 'https://cdn.mos.cms.futurecdn.net/KrWRL9o3zDChkxvEFDLRh7-970-80.jpg.webp', FALSE, 10);
+('OlliOlli World', 'Private Division', 'Switch', 'What makes OlliOlli World uniquely memorable is its slacker-utopia aesthetic. Who would not want to live on a planet that can be navigated only by kickflips?', 3000, 'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.0/c_scale,w_1100/ncom/en_US/games/switch/o/olliolli-world-switch/hero', TRUE, 1);
+('Ghostwire: Tokyo', 'Bethesda Softworks', 'Windows PC', 'This is a game in which you will banish demons before stopping into an ersatz 7-Eleven for some health-restoring mochi.', 4900, 'https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2022/03/ghostwire-tokyo-review-header.jpg?w=1280&ssl=1', TRUE, 6);
+('Lego Star Wars: The Skywalker Saga', 'Warner Bros', 'PlayStation 5', 'Yes! you will play through all of Attack of the Clones, and you will like it.', 4700, 'https://assets-prd.ignimgs.com/2022/04/04/lego-star-wars-skywalker-saga-1649085951953.jpg?width=1920', TRUE, 2);
+('Dune: Spice Wars', 'Funcom', 'Windows PC', 'It is brutal, ornery, unfair, and quintessentially Dune.', 3000, 'https://cdn.vox-cdn.com/thumbor/hSykLBqiThcI3JraW4WFA0q8gQA=/0x0:1920x1080/920x613/filters:focal(807x387:1113x693):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70716204/Early_Access_Launch_Date_Banner.0.png', FALSE, 11);
+
