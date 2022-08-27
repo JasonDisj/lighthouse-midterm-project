e8936903024eb1d34168b6cf061a38d2dce59d32
JasonDisj <jasondisj@gmail.com>
2022-08-27 19:37:10 +0000

    video_game listings and reviews

---
+++ db/schema/02_video_game_listings.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS video_game_listings CASCADE;
+
+CREATE TABLE video_game_listings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  company VARCHAR(255) NOT NULL,
+  platform VARCHAR(255) NOT NULL,
+  description TEXT,
+  price INTEGER NOT NULL,
+  cover_photo_url TEXT NOT NULL,
+  is_stocked BOOLEAN NOT NULL DEFAULT TRUE
+  admin_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
