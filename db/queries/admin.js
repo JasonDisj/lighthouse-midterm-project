0c97e71a6ee0f0a1d6a13c3a2f5c51641f398fbf
JasonDisj <jasondisj@gmail.com>
2022-09-01 21:24:37 +0000

    get admin in users table

--- db/queries/admin.js
+++ db/queries/admin.js
@@ -1,11 +1,15 @@
 const db = require('../connection');
 
+// find who is an admin from user table
+
 const getUsers = (id) => {
   return db
     .query('SELECT * FROM users WHERE id = $1;', [id])
+
     .then((result) => {
       return result.rows[0];
     })
+
     .catch((err) => {
       console.log('error', err);
     })
