b5a7a695ff5205d7499397289646af9cd94488ce
JasonDisj <jasondisj@gmail.com>
2022-09-01 21:56:28 +0000

    show favourite listings

--- db/queries/showFav.js
+++ db/queries/showFav.js
@@ -1,16 +1,17 @@
 const db = require('../connection');
 
-
-const showFav = function(customer_id) {
+const showFav = function (customer_id) {
   return db
     .query(`SELECT * FROM favourites
     JOIN video_game_listings ON video_game_id = video_game_listings.id
     WHERE customer_id = $1
     `,
       [customer_id])
+
     .then((result) => {
       return result.rows;
     })
+
     .catch((err) => {
       console.log('error', err);
     })
