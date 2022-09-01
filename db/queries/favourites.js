3f7f0a216a7a0e86767ff8612546cd732298b1a2
JasonDisj <jasondisj@gmail.com>
2022-09-01 21:34:00 +0000

    users have favourite listings

--- db/queries/favourites.js
+++ db/queries/favourites.js
@@ -1,16 +1,18 @@
 const db = require('../connection');
 
-
-const favouriteListing = function(favouriteGame, customer_id) {
+// all users can have a favourite listing
+const favouriteListing = function (favouriteGame, customer_id) {
   console.log(customer_id, favouriteGame);
   return db
     .query(`INSERT INTO favourites (customer_id, video_game_id) VALUES
     ($1, $2) RETURNING *;`,
       [customer_id, favouriteGame])
+
     .then((result) => {
       console.log('result', result.rows);
       return result.rows[0];
     })
+
     .catch((err) => {
       console.log('error', err);
     })
