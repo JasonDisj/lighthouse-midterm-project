b7db5fa070a7bfae4c29813e66d46b1e1c5b0470
John Sweeney <jes2806@gmail.com>
2022-09-01 13:47:40 -0600

    adding bug fixes

--- db/schema/03_video_game_reviews.sql
+++ db/schema/03_video_game_reviews.sql
@@ -4,6 +4,6 @@ CREATE TABLE video_game_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   video_game_id INTEGER REFERENCES video_game_listings(id) ON DELETE CASCADE,
   customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  rating SMALLINT NOT NULL DEFAULT 0,
+  rating TEXT DEFAULT 'No ratings available',
   comment TEXT
 );
