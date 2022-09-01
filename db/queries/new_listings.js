62b2ea9b6f219436b3988b97549a6b68200444ff
JasonDisj <jasondisj@gmail.com>
2022-09-01 21:44:26 +0000

    admin can add new listings

--- db/queries/new_listings.js
+++ db/queries/new_listings.js
@@ -1,7 +1,8 @@
 const db = require('../connection');
 
+// ADMIN: add new listings
 
-const addListing = function(game, userId) {
+const addListing = function (game, userId) {
   return db
     .query(`INSERT INTO video_game_listings (name, company, platform, description, price, cover_photo_url, admin_id) VALUES
          ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
@@ -9,6 +10,7 @@ const addListing = function(game, userId) {
     .then((result) => {
       return result.rows[0];
     })
+
     .catch((err) => {
       console.log('error', err);
     })
