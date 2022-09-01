97673086b0109b9d371a942c9a87a6509090bfa4
JasonDisj <jasondisj@gmail.com>
2022-09-01 21:27:03 +0000

    show user's email

--- db/queries/contact.js
+++ db/queries/contact.js
@@ -1,17 +1,17 @@
 const db = require('../connection');
 
-// ADMIN: show all listings
+// show user's email
 const showEmail = (userId) => {
   return db
     .query(`SELECT email FROM users
     WHERE id = $1;`, [userId]
     )
+
     .then((result) => {
       console.log(result.rows);
-      // console.log(result.rows[0]);
       return result.rows[0].email;
-
     })
+
     .catch((err) => {
       console.log('error', err);
     })
